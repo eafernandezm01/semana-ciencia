c7a6aab83d1a313191989fcf6896c8966064fa48
nparejom04 <nparejom04@educarex.es>
2025-11-05 09:22:30 +0100

    ,

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Juego } from './juego/juego';
+import { Inicio } from './inicio/inicio';
+
 
 export const routes: Routes = [
-    {path: 'juego', component: Juego}
+    {path: 'juego', component: Juego},
+    {path: 'inicio', component: Inicio}
 ];
