5b7433f56adba06e7cfc5154319cb228e3e7a968
Juan Carlos G. C. <jcgarciac01@educarex.es>
2025-11-07 11:15:45 +0100

    navbar - i feel bonita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { Juego } from './juego/juego';
-import { Inicio } from './inicio/inicio';
-import { Funcionamiento } from './funcionamiento/funcionamiento';
+
 
 export const routes: Routes = [
     {path: 'juego', component: Juego},
-    {path: 'inicio', component: Inicio},
-    {path: 'funcionamiento', component: Funcionamiento},
+
+    
 ];
