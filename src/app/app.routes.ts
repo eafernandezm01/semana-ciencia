dceb4fe6aeca16c1b60a1deed7e13b063d4be1c9
Juan Carlos G. C. <jcgarciac01@educarex.es>
2025-11-07 10:50:17 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Juego } from './juego/juego';
+import { Inicio } from './inicio/inicio';
+import { Funcionamiento } from './funcionamiento/funcionamiento';
 
 export const routes: Routes = [
     {path: 'juego', component: Juego},
-    //{path: 'inicio', component: Inicio},
-    //{path: 'funcionamiento', component:Funcionamiento},
+    {path: 'inicio', component: Inicio},
+    {path: 'funcionamiento', component: Funcionamiento},
 ];
