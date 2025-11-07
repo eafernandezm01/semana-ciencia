1e9093f43d7f5b3eb0f040d64f5489cfbd0169fd
Adrian <azamudioz01@educarex.es>
2025-11-07 13:36:24 +0100

    Creación de imagnes de logo

--- src/app/pages/creditos/creditos.ts
+++ src/app/pages/creditos/creditos.ts
@@ -10,44 +10,158 @@ import { Grupo, TRABAJO } from '../../interface/Grupo';
 })
 export class Creditos {
 
-  grupos:Array<Grupo> = [
+  grupos: Array<Grupo> = [
     {
       alumnos: [
         {
           nombre: "José Antonio",
           apellidos: "Calderón Pineda",
-          esLider:true
+          esLider: true
         },
         {
           nombre: "José Ángel",
           apellidos: "Casado Chamizo",
-          esLider:false
+          esLider: false
         },
         {
           nombre: "Esteban Ismael",
           apellidos: "Fernández Olid",
-          esLider:false
+          esLider: false
         },
         {
           nombre: "Alejandro",
           apellidos: "López Rodríguez",
-          esLider:false
+          esLider: false
         },
         {
           nombre: "Adrián",
           apellidos: "Zamudio",
-          esLider:false
+          esLider: false
         }
       ],
       logo: "logoEquipoJose.png", // TODO Añadir logo
       trabajo: TRABAJO.CREDITOS
     },
+    {
+      alumnos: [
+        {
+          nombre: "Juan Carlos",
+          apellidos: "García Cabrera",
+          esLider: true
+        },
+        {
+          nombre: "Steven",
+          apellidos: "Araujo Gutiérrez",
+          esLider: false
+        },
+        {
+          nombre: "Anaya",
+          apellidos: "Vegas Velázquez",
+          esLider: false
+        },
+        {
+          nombre: "Jose Diego",
+          apellidos: "Sánchez Moro",
+          esLider: false
+        },
+        {
+          nombre: "Gabriel",
+          apellidos: "Manchado Murillo",
+          esLider: false
+        }
+      ],
+      logo: "", // TODO Añadir logo
+      trabajo: TRABAJO.CREDITOS
+    },
+    {
+      alumnos: [
+        {
+          nombre: "Jorge",
+          apellidos: "Vaquero Haba",
+          esLider: true
+        },
+        {
+          nombre: "Miguel Angel",
+          apellidos: "Bahos Roldan",
+          esLider: false
+        },
+        {
+          nombre: "David",
+          apellidos: "Fruto Olivares",
+          esLider: false
+        },
+        {
+          nombre: "Victor",
+          apellidos: "Dominguéz Carmona",
+          esLider: false
+        }
+      ],
+      logo: "", // TODO Añadir logo
+      trabajo: TRABAJO.CREDITOS
+    },
+    {
+      alumnos: [
+        {
+          nombre: "Carlos",
+          apellidos: "Parejo Nieto",
+          esLider: true
+        },
+        {
+          nombre: "Emma",
+          apellidos: "Donoso Velarde",
+          esLider: false
+        },
+        {
+          nombre: "Gonzalo",
+          apellidos: "Gómez Sánchez",
+          esLider: false
+        },
+        {
+          nombre: "Ismael",
+          apellidos: "García Trejo",
+          esLider: false
+        }
+      ],
+      logo: "", // TODO Añadir logo
+      trabajo: TRABAJO.CREDITOS
+    },
+    {
+      alumnos: [
+        {
+          nombre: "Natalia",
+          apellidos: "Parejo Martins",
+          esLider: true
+        },
+        {
+          nombre: "Hector",
+          apellidos: "Sanchez Requero",
+          esLider: false
+        },
+        {
+          nombre: "Alex",
+          apellidos: "Parejo Dorado",
+          esLider: false
+        },
+        {
+          nombre: "Marcos Andres",
+          apellidos: "Jiménez Rodríguez",
+          esLider: false
+        },
+        {
+          nombre: "Raul",
+          apellidos: "Carrasco González",
+          esLider: false
+        }
+      ],
+      logo: "", // TODO Añadir logo
+      trabajo: TRABAJO.CREDITOS
+    }
 
 
     //logo Juan Carlos es josex 
-    
+
     // TODO Crear el resto de grupos con su respectiva información 
   ]
 
-  
+
 }
